4def029120d4692f78d454698081dc66eedac0b8
sifecs <sifecs15@mail.ru>
2020-03-13 15:51:18 +0300

    changed Change IPA Routes

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use App\User;
 use Illuminate\Http\Request;
 
-Route::group(['middleware' =>'auth:api'], function (){
-    Route::GET('/balans', 'Api\V1\ApiControler@getBalans');
-    Route::GET('/posts', 'Api\V1\PostsController@index');
-    Route::POST('/posts', 'Api\V1\PostsController@store');
-    Route::PUT('/posts/{post}', 'Api\V1\PostsController@update');
-    Route::DELETE('/posts/{post}', 'Api\V1\PostsController@destroy');
+Route::group(['prefix'=>'V1', 'namespace'=>'Api\V1', 'middleware' =>'auth:api'], function (){
+    Route::GET('/balans', 'ApiControler@getBalans');
+    Route::GET('/posts', 'PostsController@index');
+    Route::POST('/posts', 'PostsController@store');
+    Route::PUT('/posts/{post}', 'PostsController@update');
+    Route::DELETE('/posts/{post}', 'PostsController@destroy');
 //    Route::resource('/posts', 'Api\V1\PostsController');
 });
