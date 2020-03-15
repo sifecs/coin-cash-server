dbbb5ee365d94453749466361f2f54c75a4101ef
sifecs <sifecs15@mail.ru>
2020-03-15 21:21:59 +0300

    deleted directory V1

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use App\User;
 use Illuminate\Http\Request;
 
-Route::group(['prefix'=>'V1', 'namespace'=>'Api\V1', 'middleware' =>'auth:api'], function (){
+Route::group(['prefix'=>'V1', 'namespace'=>'Api', 'middleware' =>'auth:api'], function (){
     Route::GET('/balans', 'ApiControler@getBalans');
     Route::GET('/finance', 'FinanceController@index');
     Route::POST('/finance', 'FinanceController@store');
+    Route::PUT('/finance/{financ}', 'FinanceController@update');
     Route::PUT('/finance/{financ}', 'FinanceController@update');
     Route::DELETE('/finance/{financ}', 'FinanceController@destroy');
 //    Route::resource('/finance', 'FinanceController');
